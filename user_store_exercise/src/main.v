3b601b3b351c800bdc3b4b87483eba88ee14fe53
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-04-14 21:04:35 +0100

    testing closure and signal ctrl+c handling

--- user_store_exercise/src/main.v
+++ user_store_exercise/src/main.v
@@ -1,5 +1,43 @@
 module main
 
+import os
+
+struct App {
+mut:
+	last string
+}
+
+fn (mut app App) run() {
+	mut choice := ''
+	for {
+		println('${app.last}')
+		choice = os.input('gimme: ')
+
+		if choice == 'q' {
+			break
+		}
+		app.last = choice
+	}
+
+	app.cleanup()
+}
+
+fn (app App) cleanup() {
+	println('cleaning up...\n\n')
+	println('had ${app.last}')
+	println('done!')
+}
+
 fn main() {
-	println('Hello World!')
+	mut app := &App{}
+	cleanup := fn [app] (_ os.Signal) {
+		println('\n\nCtrl-C was pressed')
+		app.cleanup()
+		println('finished. bye!')
+		exit(0)
+	}
+	// .int is the same as os.Signal.int
+	os.signal_opt(.int, cleanup) or { println("can't setup handler") }
+
+	app.run()
 }
